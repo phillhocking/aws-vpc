7291ef35dd32f4b69ad9bd241cbf72f39b3904a4
Phillip Hocking <phillhocking@gmail.com>
2021-10-18 16:19:13 -0700

    Fix tflint style issues from failed action

--- vpc/main.tf
+++ vpc/main.tf
@@ -11,8 +11,8 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  tags                 = {
-    Name               = var.vpc_name
+  tags = {
+    Name = var.vpc_name
   }
 }
 
@@ -24,11 +24,11 @@ resource "aws_subnet" "dev" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(var.cidr_block, 6, 2)
 
-  tags              = {
-    Name            = "dev-subnet"
+  tags = {
+    Name = "dev-subnet"
   }
 
-  depends_on        = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
@@ -40,11 +40,11 @@ resource "aws_subnet" "staging" {
   cidr_block        = cidrsubnet(var.cidr_block, 6, 1)
 
 
-  tags              = {
-    Name            = "staging-subnet"
+  tags = {
+    Name = "staging-subnet"
   }
 
-  depends_on        = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
@@ -55,27 +55,27 @@ resource "aws_subnet" "prod" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(var.cidr_block, 6, 0)
 
-  tags              = {
-    Name            = "prod-subnet"
+  tags = {
+    Name = "prod-subnet"
   }
 
-  depends_on        = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
 
 
 resource "aws_subnet" "public" {
-# This line is necessary to ensure that we pick availabiltiy zones that can launch any size ec2 instance
+  # This line is necessary to ensure that we pick availabiltiy zones that can launch any size ec2 instance
   availability_zone = local.aws_available_zones
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 254)
 
- tags               = {
-    Name            = "public-subnet"
+  tags = {
+    Name = "public-subnet"
   }
 
-  depends_on        = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
@@ -88,7 +88,7 @@ resource "aws_network_acl" "vpc" {
   subnet_ids = concat(
     [aws_subnet.prod.id],
     [aws_subnet.staging.id],
-    [aws_subnet.dev.id], 
+    [aws_subnet.dev.id],
     [aws_subnet.public.id]
   )
 
@@ -110,24 +110,24 @@ resource "aws_network_acl" "vpc" {
     to_port    = 0
   }
 
-  tags         = {
-    Name       = "${var.vpc_name}-nacl"
+  tags = {
+    Name = "${var.vpc_name}-nacl"
   }
 
-  depends_on   = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
 
 # Gateways
 resource "aws_internet_gateway" "gw" {
-  vpc_id      = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-  tags        = {
-    Name      = "${var.vpc_name}-internet-gateway"
+  tags = {
+    Name = "${var.vpc_name}-internet-gateway"
   }
 
-  depends_on  = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
@@ -136,11 +136,11 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_default_route_table" "default" {
   default_route_table_id = aws_vpc.main.main_route_table_id
 
-  tags                   = {
+  tags = {
     Name = "${var.vpc_name}-public"
   }
 
-  depends_on            = [
+  depends_on = [
     aws_internet_gateway.gw,
     aws_vpc.main,
   ]
@@ -148,13 +148,13 @@ resource "aws_default_route_table" "default" {
 
 # prod Subnet Route Table
 resource "aws_route_table" "prod" {
-  vpc_id    = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-  tags      = {
-    Name    = "prod-route-table"
+  tags = {
+    Name = "prod-route-table"
   }
 
-  depends_on = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
@@ -163,7 +163,7 @@ resource "aws_route_table_association" "prod_routes" {
   subnet_id      = aws_subnet.prod.id
   route_table_id = aws_route_table.prod.id
 
-  depends_on     = [
+  depends_on = [
     aws_internet_gateway.gw,
     aws_subnet.prod,
   ]
@@ -174,7 +174,7 @@ resource "aws_route" "prod_gw" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 
-  depends_on             = [
+  depends_on = [
     aws_route_table.prod,
     aws_internet_gateway.gw,
   ]
@@ -182,13 +182,13 @@ resource "aws_route" "prod_gw" {
 
 # Staging Subnet Route Table
 resource "aws_route_table" "staging" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-  tags       = {
-    Name     = "staging-route-table"
+  tags = {
+    Name = "staging-route-table"
   }
 
-  depends_on = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
@@ -197,7 +197,7 @@ resource "aws_route_table_association" "staging_routes" {
   subnet_id      = aws_subnet.staging.id
   route_table_id = aws_route_table.staging.id
 
-  depends_on     = [
+  depends_on = [
     aws_internet_gateway.gw,
     aws_subnet.staging,
     aws_route_table.staging,
@@ -209,7 +209,7 @@ resource "aws_route" "staging_igw" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 
-  depends_on             = [
+  depends_on = [
     aws_internet_gateway.gw,
     aws_route_table.staging,
   ]
@@ -217,13 +217,13 @@ resource "aws_route" "staging_igw" {
 
 # Dev Subnet Route Table
 resource "aws_route_table" "dev" {
-  vpc_id      = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-  tags        = {
-    Name      = "dev-route-table"
+  tags = {
+    Name = "dev-route-table"
   }
 
-  depends_on  = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
@@ -232,7 +232,7 @@ resource "aws_route_table_association" "dev_routes" {
   subnet_id      = aws_subnet.dev.id
   route_table_id = aws_route_table.dev.id
 
-  depends_on     = [
+  depends_on = [
     aws_internet_gateway.gw,
     aws_route_table.dev,
   ]
@@ -243,7 +243,7 @@ resource "aws_route" "dev_igw" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 
-  depends_on             = [
+  depends_on = [
     aws_internet_gateway.gw,
     aws_route_table.dev,
   ]
@@ -251,13 +251,13 @@ resource "aws_route" "dev_igw" {
 
 # Public Subnet Route Table
 resource "aws_route_table" "public" {
-  vpc_id    = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-  tags      = {
-    Name    = "public-route-table"
+  tags = {
+    Name = "public-route-table"
   }
 
-  depends_on = [ 
+  depends_on = [
     aws_vpc.main,
   ]
 }
@@ -266,18 +266,18 @@ resource "aws_route_table_association" "public_routes" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 
-  depends_on     = [
+  depends_on = [
     aws_internet_gateway.gw,
     aws_route_table.public,
   ]
 }
 
 resource "aws_route" "public_igw" {
-  route_table_id          = aws_route_table.public.id
-  destination_cidr_block  = "0.0.0.0/0"
-  gateway_id              = aws_internet_gateway.gw.id
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
 
-  depends_on              = [
+  depends_on = [
     aws_internet_gateway.gw,
     aws_route_table.public,
   ]
